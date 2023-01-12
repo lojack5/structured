26553f84e01883c3d5c31aefd9edefc1889083a8
lojack5 <1458329+lojack5@users.noreply.github.com>
2023-01-11 23:00:32 -0700

    tests: refactor out common testing code

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,25 @@
 import io
 
-from structured import *
+from structured import Structured
 
 
 def standard_tests(target_obj: Structured, target_data: bytes):
+    target_size = len(target_data)
     assert target_obj.pack() == target_data
     assert type(target_obj).create_unpack(target_data) == target_obj
+    assert target_obj.serializer.size == target_size
 
     buffer = bytearray(len(target_data))
     target_obj.pack_into(buffer)
     assert bytes(buffer) == target_data
+    assert target_obj.serializer.size == target_size
     assert type(target_obj).create_unpack_from(buffer) == target_obj
+    assert target_obj.serializer.size == target_size
 
     with io.BytesIO() as stream:
         target_obj.pack_write(stream)
+        assert target_obj.serializer.size == target_size
         assert stream.getvalue() == target_data
         stream.seek(0)
         assert type(target_obj).create_unpack_read(stream) == target_obj
+        assert target_obj.serializer.size == target_size
