5053adf05dfd43ffe3cac5f9863aeb9526149d5b
lojack5 <1458329+lojack5@users.noreply.github.com>
2024-10-03 22:05:26 -0600

    ConditionalSerializer and hint Condition - Hint used to only pack/unpack a variable when a condition is met - Unions can't quite accomplish this correctly

---
+++ structured/serializers/conditional.py
@@ -0,0 +1,68 @@
+"""
+Serializer that wraps another in a condition. When the condition evaluates
+to a Truthy value, the original serializer operates as normal. Otherwise,
+it acts as if the serializer was not there.
+"""
+__all__ = [
+    'ConditionalSerializer',
+]
+
+from .api import Serializer, ByteOrder
+from ..type_checking import Ts, Unpack, Generic, Callable, TYPE_CHECKING, TypeVar, Any, WritableBuffer, ReadableBuffer, BinaryIO, Self
+
+if TYPE_CHECKING:
+    # *Only* used for type-hinting, so ok to guard with a TYPE_CHECKING
+    from ..structured import Structured
+
+    TStructured = TypeVar('TStructured', bound=Structured)
+else:
+    TStructured = TypeVar('TStructured')
+
+
+class ConditionalSerializer(Generic[Unpack[Ts]], Serializer[Unpack[Ts]]):
+    def __init__(self, serializer: Serializer[Unpack[Ts]], condition: Callable[[TStructured], bool], default: tuple[Unpack[Ts]]) -> None:
+        self.condition = condition
+        self.serializers: dict[bool, Serializer[Unpack[Ts]]] = {
+            True: serializer,
+            False: SkipSerializer(default)
+        }
+        self.num_values = serializer.num_values
+        if serializer.num_values != len(default):
+            raise ValueError('Not enough default arguments')
+        
+    def with_byte_order(self, byte_order: ByteOrder) -> Self:
+        serializer = self.serializers[True].with_byte_order(byte_order)
+        defaults = self.serializers[False].values
+        return type(self)(serializer, self.condition, defaults)
+
+    def prepack(self, partial_object: Any) -> Serializer[Unpack[Ts]]:
+        return self.serializers[self.condition(partial_object)]
+        
+    def preunpack(self, partial_object: Any) -> Serializer[Unpack[Ts]]:
+        return self.prepack(partial_object)
+    
+
+class SkipSerializer(Generic[Unpack[Ts]], Serializer[Unpack[Ts]]):
+    size: int = 0
+
+    def __init__(self, values: tuple[Unpack[Ts]]):
+        self.values = values
+        self.num_values = len(values)
+
+    def pack(self, *values: Unpack[Ts]) -> bytes:
+        return b''
+    
+    def pack_into(self, buffer: WritableBuffer, offset: int, *values: Unpack[Ts]) -> None:
+        pass
+
+    def pack_write(self, writable: BinaryIO, *values: Unpack[Ts]) -> None:
+        pass
+
+    def unpack(self, buffer: ReadableBuffer) -> tuple[Unpack[Ts]]:
+        return self.values
+        
+    def unpack_from(self, buffer: ReadableBuffer, offset: int = 0) -> tuple[Unpack[Ts]]:
+        return self.values
+    
+    def unpack_read(self, readable: BinaryIO) -> tuple:
+        return self.values
